55a80338d2ed754995a7a94f40249d2481c16ebf
giuxtaposition <yg97.cs@gmail.com>
2023-08-15 21:42:57 +0200

    feat: add scripts to quickly install and setup

---
+++ scripts/install-packages.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+SCRIPT_DIR_ABS=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+install_file=$SCRIPT_DIR_ABS/pkglist.txt
+log_file=$SCRIPT_DIR_ABS/install_progress_log.txt
+
+#==============
+# Ask for sudo privileges
+#==============
+if [ "$EUID" != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+#==============
+# Install packages
+#==============
+packages=()
+while IFS= read -r line; do
+   packages+=("$line")
+done <$install_file
+
+yay -S --noconfirm --needed "${packages[*]}" &> /dev/null
+
+function print_package_status {
+  if command -v $1 &> /dev/null
+    echo "$1 installed" >> $log_file
+  then
+    echo "$1 failed to install" >> $log_file
+  fi
+}
+
+for t in ${packages[@]}; do
+  print_package_status $t
+done
+
+#==============
+# Installation Summary
+#==============
+echo -e "\n====== Summary ======\n"
+cat $log_file
+echo
+rm $log_file
