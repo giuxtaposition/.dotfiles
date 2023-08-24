dda92ca0f358bde5f6e4fe074d01e9fff84c2e18
giuxtaposition <yg97.cs@gmail.com>
2023-08-24 18:56:50 +0200

    feat(neovim): better looking bufferline

--- .config/nvim/lua/giuxtaposition/plugins/bufferline.lua
+++ .config/nvim/lua/giuxtaposition/plugins/bufferline.lua
@@ -6,11 +6,15 @@ end
 bufferline.setup({
 	options = {
 		mode = "tabs",
-		separator_style = "slant",
+		separator_style = "thin",
 		always_show_bufferline = false,
-		show_buffer_close_icons = false,
-		show_close_icon = false,
+		show_buffer_close_icons = true,
+		show_close_icon = true,
 		color_icons = true,
+		indicator = {
+			style = "none",
+		},
+		show_tab_indicators = true,
 	},
 	highlights = require("catppuccin.groups.integrations.bufferline").get(),
 })
