96a673079621a5db0840928e9392135be20bdcc0
giuxtaposition <yg97.cs@gmail.com>
2023-09-04 13:57:20 +0200

    fix(neovim): neo-test config

--- .config/nvim/lua/giuxtaposition/plugins/neo-test.lua
+++ .config/nvim/lua/giuxtaposition/plugins/neo-test.lua
@@ -1,5 +1,6 @@
 return {
 	{ "nvim-neotest/neotest-plenary" },
+	{ "haydenmeade/neotest-jest" },
 	{
 		"nvim-neotest/neotest",
 		dependencies = {
@@ -8,35 +9,79 @@ return {
 			"antoinemadec/FixCursorHold.nvim",
 			"nvim-neotest/neotest-plenary",
 		},
-		opts = {
-			adapters = {
-				"neotest-plenary",
-				{
-					"neotest-jest",
-					{
-						jestCommand = "npm run test --",
+
+		config = function()
+			local neotest = require("neotest")
+
+			neotest.setup({
+				library = { plugins = { "neotest" }, types = true },
+				adapters = {
+					require("neotest-jest")({
+						jestCommand = "npm test --",
 						jestConfigFile = "custom.jest.config.ts",
 						env = { CI = true },
-						cwd = function(_)
+						cwd = function()
 							return vim.fn.getcwd()
 						end,
-					},
+					}),
 				},
+			})
+		end,
+		-- config = function(_, opts)
+		-- 	local neotest_ns = vim.api.nvim_create_namespace("neotest")
+		-- 	vim.diagnostic.config({
+		-- 		virtual_text = {
+		-- 			format = function(diagnostic)
+		-- 				-- Replace newline and tab characters with space for more compact diagnostics
+		-- 				local message =
+		-- 					diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+		-- 				return message
+		-- 			end,
+		-- 		},
+		-- 	}, neotest_ns)
+		--
+		-- 	if opts.adapters then
+		-- 		local adapters = {}
+		-- 		for name, config in pairs(opts.adapters or {}) do
+		-- 			if type(name) == "number" then
+		-- 				if type(config) == "string" then
+		-- 					config = require(config)
+		-- 				end
+		-- 				adapters[#adapters + 1] = config
+		-- 			elseif config ~= false then
+		-- 				local adapter = require(name)
+		-- 				if type(config) == "table" and not vim.tbl_isempty(config) then
+		-- 					local meta = getmetatable(adapter)
+		-- 					if adapter.setup then
+		-- 						adapter.setup(config)
+		-- 					elseif meta and meta.__call then
+		-- 						adapter(config)
+		-- 					else
+		-- 						error("Adapter " .. name .. " does not support setup")
+		-- 					end
+		-- 				end
+		-- 				adapters[#adapters + 1] = adapter
+		-- 			end
+		-- 		end
+		-- 		opts.adapters = adapters
+		-- 	end
+		--
+		-- 	require("neotest").setup(opts)
+		-- end,
+		keys = {
+			{ "<leader>tr", "<cmd>lua require('neotest').run.run()<cr>", desc = "Run nearest test" },
+			{
+				"<leader>tw",
+				"<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>",
+				desc = "Run tests in current file",
 			},
-			keys = {
-				{ "<leader>tr", "<cmd>lua require('neotest').run.run()<cr>", desc = "Run nearest test" },
-				{
-					"<leader>tw",
-					"<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>",
-					desc = "Run tests in current file",
-				},
-				{ "<leader>tl", "<cmd>lua require('neotest').run.run_last()<cr>", desc = "Run last test" },
-				{ "<leader>ts", "<cmd>lua require('neotest').summary.toggle()<cr>", desc = "Toggle tests summary" },
-				{
-					"<leader>tO",
-					"<cmd>lua require('neotest').output_panel.toggle()<cr>",
-					desc = "Toggle tests output panel",
-				},
+			{ "<leader>tW", "<cmd>lua require('neotest').run.run(vim.loop.cwd())<cr>", desc = "Run all test files" },
+			{ "<leader>tl", "<cmd>lua require('neotest').run.run_last()<cr>", desc = "Run last test" },
+			{ "<leader>ts", "<cmd>lua require('neotest').summary.toggle()<cr>", desc = "Toggle tests summary" },
+			{
+				"<leader>tO",
+				"<cmd>lua require('neotest').output_panel.toggle()<cr>",
+				desc = "Toggle tests output panel",
 			},
 		},
 	},
