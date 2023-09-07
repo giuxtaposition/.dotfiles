3a5e84b4a716e708541cb8e3fa356f64107cede1
giuxtaposition <yg97.cs@gmail.com>
2023-09-07 15:40:33 +0200

    feat(neovim): use lazygit instead of neogit

--- .config/nvim/lua/giuxtaposition/plugins/toggleterm.lua
+++ .config/nvim/lua/giuxtaposition/plugins/toggleterm.lua
@@ -8,8 +8,30 @@ return {
 		autochdir = true, -- auto change dir following neovim
 		insert_mapping = true, -- open terminal mapping works also in insert mode
 	},
+	config = function(_, opts)
+		require("toggleterm").setup(opts)
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({
+			cmd = "lazygit",
+			hidden = true,
+			direction = "float",
+			float_opts = {
+				border = "double",
+			},
+			-- function to run on opening the terminal
+			on_open = function(term)
+				vim.cmd("startinsert!")
+				vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+			end,
+		})
+
+		function _lazygit_toggle()
+			lazygit:toggle()
+		end
+	end,
 	keys = {
 		{ "<leader>tth", "<cmd>ToggleTerm size=15 direction=horizontal<cr>", desc = "Horizontal Terminal" },
 		{ "<leader>ttv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "Vertical Terminal" },
+		{ "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", desc = "Lazygit Terminal" },
 	},
 }
