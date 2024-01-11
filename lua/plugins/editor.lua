5d72753daed5e1a6d248bc1e936dae22e5c0386e
giuxtaposition <yg97.cs@gmail.com>
2024-01-11 17:27:17 +0100

    feat(nvim): update editor styling

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -3,16 +3,14 @@ return {
     "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      -- disable netrw as suggested by nvim-tree
-      vim.g.loaded_netrw = 1
-      vim.g.loaded_netrwPlugin = 1
-
-      -- change color for arrows in tree
       vim.cmd([[ highlight NvimTreeIndentMarker guifg=#6c7086]])
 
       local nvimtree = require("nvim-tree")
 
       nvimtree.setup({
+        disable_netrw = true,
+        hijack_netrw = true,
+        hijack_cursor = true,
         diagnostics = {
           enable = true,
           icons = {
@@ -22,12 +20,36 @@ return {
             error = "",
           },
         },
+        filesystem_watchers = {
+          enable = true,
+        },
+        actions = {
+          open_file = {
+            resize_window = true,
+            quit_on_open = true,
+          },
+        },
         renderer = {
+          root_folder_label = false,
           icons = {
+            show = {
+              file = true,
+              folder = true,
+              folder_arrow = true,
+              git = false,
+            },
             glyphs = {
+              default = "󰈚",
+              symlink = "",
               folder = {
-                arrow_closed = "", -- when folder is closed
-                arrow_open = "", -- when folder is open
+                default = "",
+                empty = "",
+                empty_open = "",
+                open = "",
+                symlink = "",
+                symlink_open = "",
+                arrow_open = "",
+                arrow_closed = "",
               },
               git = {
                 unstaged = "✗",
@@ -39,14 +61,6 @@ return {
                 ignored = "◌",
               },
             },
-          },
-        },
-        actions = {
-          open_file = {
-            window_picker = {
-              enable = false,
-            },
-            quit_on_open = true,
           },
         },
         update_cwd = true,
@@ -72,6 +86,32 @@ return {
         require("telescope").load_extension("fzf")
       end,
     },
+    opts = {
+      defaults = {
+        prompt_prefix = "   ",
+        selection_caret = "  ",
+        entry_prefix = "  ",
+        layout_strategy = "horizontal",
+        layout_config = {
+          horizontal = {
+            prompt_position = "top",
+            preview_width = 0.55,
+            results_width = 0.8,
+          },
+          vertical = {
+            mirror = false,
+          },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 120,
+        },
+        file_ignore_patterns = { "node_modules" },
+        winblend = 0,
+        border = {},
+        borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+        color_devicons = true,
+      },
+    },
     keys = {
       {
         "<leader>fx",
@@ -89,5 +129,18 @@ return {
     "ecthelionvi/NeoComposer.nvim",
     dependencies = { "kkharji/sqlite.lua" },
     opts = {},
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = "│" },
+        change = { text = "│" },
+        delete = { text = "󰍵" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "│" },
+      },
+    },
   },
 }
