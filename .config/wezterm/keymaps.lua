78443384e9f7cae4ba2ce5ac94252f33a5ce82ac
giuxtaposition <yg97.cs@gmail.com>
2024-01-26 19:11:29 +0100

    fix(wezterm): smart splits no longer working

--- .config/wezterm/keymaps.lua
+++ .config/wezterm/keymaps.lua
@@ -1,44 +1,7 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
+
 local module = {}
-
-local function is_vim(pane)
-  -- this is set by the plugin, and unset on ExitPre in Neovim
-  return pane:get_user_vars().IS_NVIM == "true" or pane:get_foreground_process_name():find("n?vim")
-end
-
-local direction_keys = {
-  Left = "h",
-  Down = "j",
-  Up = "k",
-  Right = "l",
-  -- reverse lookup
-  h = "Left",
-  j = "Down",
-  k = "Up",
-  l = "Right",
-}
-
-local function split_nav(resize_or_move, key)
-  return {
-    key = key,
-    mods = resize_or_move == "resize" and "META" or "CTRL",
-    action = wezterm.action_callback(function(win, pane)
-      if is_vim(pane) then
-        -- pass the keys through to vim/nvim
-        win:perform_action({
-          SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
-        }, pane)
-      else
-        if resize_or_move == "resize" then
-          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-        else
-          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-        end
-      end
-    end),
-  }
-end
 
 function module.apply_to_config(config)
   config.disable_default_key_bindings = true
@@ -57,18 +20,6 @@ function module.apply_to_config(config)
       mods = "LEADER",
       action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
     },
-
-    -- move between split panes
-    split_nav("move", "h"),
-    split_nav("move", "j"),
-    split_nav("move", "k"),
-    split_nav("move", "l"),
-
-    -- resize panes
-    split_nav("resize", "h"),
-    split_nav("resize", "j"),
-    split_nav("resize", "k"),
-    split_nav("resize", "l"),
 
     { key = "q", mods = "LEADER", action = act.CloseCurrentPane({ confirm = false }) },
     { key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require("wezterm")
 local theme = require("theme")
 local keymaps = require("keymaps")
+local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
 
 local config = {}
 config = wezterm.config_builder()
@@ -9,5 +10,12 @@ config.default_workspace = "home"
 
 theme.apply_to_config(config)
 keymaps.apply_to_config(config)
+smart_splits.apply_to_config(config, {
+  direction_keys = { "h", "j", "k", "l" },
+  modifiers = {
+    move = "CTRL",
+    resize = "META",
+  },
+})
 
 return config
