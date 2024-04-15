ca23bf5d474e4febd50dbbeffaf30d0da719775c
giuxtaposition <yg97.cs@gmail.com>
2024-04-15 09:49:49 +0200

    feat(eww): add bluetooth in control center

--- .config/eww/scripts/positions.sh
+++ .config/eww/scripts/positions.sh
@@ -3,12 +3,12 @@
 playerctl metadata -F -f '{{position}} {{mpris:length}}' | while read -r line; do
 	position=$(playerctl metadata -f "{{position / 1000000}}")
 	position=$(echo "($position + 0.5) / 1" | bc)
+	echo "$position"
 	positionStr=$(playerctl metadata -f "{{duration(position)}}")
 	player=$(playerctl metadata -f "{{playerName}}")
-	length=$(playerctl metadata -f "{{mpris:length}}")
+
 	JSON_STRING=$(jaq -n \
 		--arg position "$position" \
-		--arg length "$length" \
 		--arg positionStr "$positionStr" \
 		--arg player "$player" \
 		"{$player: {position: \"$position\", positionStr: \"$positionStr\"}}")
