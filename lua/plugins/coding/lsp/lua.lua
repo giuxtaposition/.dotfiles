194e58ae09b1e1368d3c7bc78f26cd2d882f2973
giuxtaposition <yg97.cs@gmail.com>
2024-10-15 21:27:49 +0200

    feat(lua): add wezterm types

--- lua/plugins/coding/lsp/lua.lua
+++ lua/plugins/coding/lsp/lua.lua
@@ -22,9 +22,11 @@ return {
     opts = {
       library = {
         { path = "luvit-meta/library", words = { "vim%.uv" } },
+        { path = "wezterm-types", mods = { "wezterm" } },
         { plugins = { "nvim-dap-ui" }, types = true },
       },
     },
   },
+  { "gonstoll/wezterm-types", lazy = true },
   { "Bilal2453/luvit-meta", lazy = true },
 }
