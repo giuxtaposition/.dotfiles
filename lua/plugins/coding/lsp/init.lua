0b540b4d31bf5ba2646d3342f83f0a093bc0b697
giuxtaposition <yg97.cs@gmail.com>
2024-10-18 10:48:44 +0200

    refactor(lsp): clean up mappings

--- lua/plugins/coding/lsp/init.lua
+++ lua/plugins/coding/lsp/init.lua
@@ -1,54 +1,69 @@
+---@param mode string|string[]
+---@param lhs string
+---@param rhs string|function
+---@param desc string
+---@param buffer boolean|integer O or true for current buffer
+---@param additional_opts? vim.keymap.set.Opts
+local map = function(mode, lhs, rhs, desc, buffer, additional_opts)
+  local opts = vim.tbl_extend("force", { buffer = buffer, desc = desc, silent = true }, additional_opts or {})
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre" },
     config = function(_, opts)
       local lspconfig = require("lspconfig")
+      require("lspconfig.ui.windows").default_options.border = "rounded"
 
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("giuxtaposition-lsp-attach", { clear = true }),
         callback = function(event)
-          local map = function(keys, func, desc, expr)
-            vim.keymap.set("n", keys, func, { buffer = event.buf, desc = desc, expr = expr, silent = true })
-          end
-
           local diagnostic_goto = function(count, severity)
             severity = severity and vim.diagnostic.severity[severity] or nil
             return function()
-              vim.diagnostic.jump({ count = count, severity = severity })
+              vim.diagnostic.jump({
+                count = count,
+                severity = severity,
+                float = { border = "rounded", max_width = 100 },
+              })
             end
           end
 
-          map("gd", require("telescope.builtin").lsp_definitions, "Goto Definition") --  To jump back, press <C-t>.
-          map("gD", vim.lsp.buf.declaration, "Goto Declaration")
-          map("gr", require("telescope.builtin").lsp_references, "Goto References")
-          map("gI", require("telescope.builtin").lsp_implementations, "Goto Implementation")
-          map("gt", require("telescope.builtin").lsp_type_definitions, "Type Definition")
-          map("<leader>cs", require("telescope.builtin").lsp_document_symbols, "Document Symbols")
-          map("<leader>cS", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Workspace Symbols")
-          map("<leader>cr", function()
+          map("n", "gd", vim.lsp.buf.definition, "Goto Definition", event.buf) --  To jump back, press <C-t>.
+          map("n", "gD", vim.lsp.buf.declaration, "Goto Declaration", event.buf)
+          map("n", "gr", function()
+            vim.lsp.buf.references({ includeDeclaration = false })
+          end, "Goto References", event.buf)
+          map("n", "gI", vim.lsp.buf.implementation, "Goto Implementation", event.buf)
+          map("n", "gt", vim.lsp.buf.type_definition, "Type Definition", event.buf)
+          map("n", "<leader>cs", vim.lsp.buf.document_symbol, "Document Symbols", event.buf)
+          map("n", "<leader>cS", vim.lsp.buf.workspace_symbol, "Workspace Symbols", event.buf)
+          map("n", "<leader>cr", function()
             local inc_rename = require("inc_rename")
             return ":" .. inc_rename.config.cmd_name .. " " .. vim.fn.expand("<cword>")
-          end, "Rename", true)
-          map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
-          map("<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", "Show buffer diagnostics")
-          map("<leader>cd", vim.diagnostic.open_float, "Show line diagnostics")
-          map("]d", diagnostic_goto(1), "Next Diagnostic")
-          map("[d", diagnostic_goto(-1), "Prev Diagnostic")
-          map("]e", diagnostic_goto(1, "ERROR"), "Next Error")
-          map("[e", diagnostic_goto(-1, "ERROR"), "Prev Error")
-          map("]w", diagnostic_goto(1, "WARN"), "Next Warning")
-          map("[w", diagnostic_goto(-1, "WARN"), "Prev Warning")
-          map("K", vim.lsp.buf.hover, "Show documentation for what is under cursor")
-          map("<leader>rs", ":LspRestart<CR>", "Restart LSP")
+          end, "Rename", event.buf, { expr = true })
+          map("n", "<leader>ca", vim.lsp.buf.code_action, "Code Action", event.buf)
+          map("v", "<leader>ca", "<cmd>'<,'>vim.lsp.buf.code_action()<CR>", "Code Action", event.buf)
+          map("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", "Show buffer diagnostics", event.buf)
+          map("n", "<leader>cd", vim.diagnostic.open_float, "Show line diagnostics", event.buf)
+          map("n", "]d", diagnostic_goto(1), "Next Diagnostic", event.buf)
+          map("n", "[d", diagnostic_goto(-1), "Prev Diagnostic", event.buf)
+          map("n", "]e", diagnostic_goto(1, "ERROR"), "Next Error", event.buf)
+          map("n", "[e", diagnostic_goto(-1, "ERROR"), "Prev Error", event.buf)
+          map("n", "]w", diagnostic_goto(1, "WARN"), "Next Warning", event.buf)
+          map("n", "[w", diagnostic_goto(-1, "WARN"), "Prev Warning", event.buf)
+          map("n", "K", vim.lsp.buf.hover, "Show documentation for what is under cursor", event.buf)
+          map("n", "<leader>rs", ":LspRestart<CR>", "Restart LSP", event.buf)
 
           local client = vim.lsp.get_client_by_id(event.data.client_id)
           -- The following code creates a keymap to toggle inlay hints in your
           -- code, if the language server you are using supports them
           if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-            map("<leader>uh", function()
+            map("n", "<leader>uh", function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-            end, "Toggle Inlay Hints")
+            end, "Toggle Inlay Hints", event.buf)
           end
         end,
       })
@@ -83,13 +98,9 @@ return {
         end
 
         if server_opts.keys then
-          server_opts.on_attach = function(client, bufnr)
-            local map = function(keys, func, desc)
-              vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
-            end
-
+          server_opts.on_attach = function(_, bufnr)
             for _, value in pairs(server_opts.keys) do
-              map(value[1], value[2], value[3])
+              map("n", value[1], value[2], value[3], bufnr)
             end
           end
         end
