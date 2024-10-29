e6bcbdf0af8f75f51930ebde5808176f08aefac4
giuxtaposition <yg97.cs@gmail.com>
2024-10-29 19:18:58 +0100

    chore: update plugins

--- lua/plugins/ui/indent-blankline.lua
+++ lua/plugins/ui/indent-blankline.lua
@@ -1,27 +1,23 @@
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-	opts = {
-		indent = {
-			char = "│",
-			tab_char = "│",
-		},
-		scope = { enabled = false },
-		exclude = {
-			filetypes = {
-				"help",
-				"alpha",
-				"dashboard",
-				"NvimTree",
-				"Trouble",
-				"trouble",
-				"lazy",
-				"mason",
-				"notify",
-				"toggleterm",
-				"lazyterm",
-			},
-		},
-	},
-	main = "ibl",
+  "lukas-reineke/indent-blankline.nvim",
+  tag = "v3.8.2",
+  event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+  opts = {
+    indent = {
+      char = "│",
+      tab_char = "│",
+    },
+    scope = { enabled = false },
+    exclude = {
+      filetypes = {
+        "help",
+        "dashboard",
+        "trouble",
+        "lazy",
+        "notify",
+        "toggleterm",
+      },
+    },
+  },
+  main = "ibl",
 }
