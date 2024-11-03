21a42f61b5965a72fcb4ab580824dcf18409f4db
giuxtaposition <yg97.cs@gmail.com>
2024-11-03 10:50:12 +0100

    feat(autocompletion): add lazydev and copilot as sources

--- lua/plugins/coding/autocompletion.lua
+++ lua/plugins/coding/autocompletion.lua
@@ -1,9 +1,15 @@
+---@module 'blink.cmp'
 return {
   {
+    -- enabled = false,
     "saghen/blink.cmp",
     lazy = false, -- lazy loading handled internally
     dependencies = {
       "rafamadriz/friendly-snippets",
+      {
+        dev = true,
+        "giuxtaposition/blink-cmp-copilot",
+      },
     },
     version = false,
     build = "nix run .#build-plugin",
@@ -22,13 +28,37 @@ return {
         ["<Tab>"] = { "snippet_forward", "fallback" },
         ["<S-Tab>"] = { "snippet_backward", "fallback" },
       },
-      nerd_font_variant = "mono",
       accept = { auto_brackets = { enabled = true } },
       trigger = { signature_help = { enabled = true } },
       windows = {
         autocomplete = {
-          draw = "reversed",
+          -- draw = "reversed",
           border = "single",
+          ---@param ctx blink.cmp.CompletionRenderContext
+          ---@returns blink.cmp.Component[]
+          draw = function(ctx)
+            local isCopilot = ctx.item.source_name == "copilot"
+
+            return {
+              " ",
+              {
+                ctx.label,
+                ctx.kind == "Snippet" and "~" or nil,
+                (ctx.item.labelDetails and ctx.item.labelDetails.detail) and ctx.item.labelDetails.detail or "",
+                fill = true,
+                hl_group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel",
+                max_width = 50,
+              },
+              " ",
+              {
+                isCopilot and require("config.ui.icons").kinds.Copilot or ctx.kind_icon,
+                ctx.icon_gap,
+                isCopilot and "Copilot" or ctx.kind,
+                hl_group = ("BlinkCmpKind" .. ctx.kind),
+              },
+              " ",
+            }
+          end,
         },
         documentation = {
           border = "single",
@@ -39,6 +69,19 @@ return {
         },
       },
       kind_icons = require("config.ui.icons").kinds,
+      sources = {
+        providers = {
+          lsp = { fallback_for = { "lazydev" } },
+          lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+          copilot = {
+            name = "copilot",
+            module = "blink-cmp-copilot",
+          },
+        },
+        completion = {
+          enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev", "copilot" },
+        },
+      },
     },
   },
   {
