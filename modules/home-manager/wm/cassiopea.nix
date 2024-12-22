61f0f69afed09d093e4b9e6559633ed2f2ef14c3
giuxtaposition <yg97.cs@gmail.com>
2024-12-22 18:07:15 +0100

    feat: ✨ update cassiopea to latest

--- modules/home-manager/wm/cassiopea.nix
+++ modules/home-manager/wm/cassiopea.nix
@@ -12,6 +12,7 @@
   config = lib.mkIf config.cassiopea.enable {
     programs.cassiopea = {
       enable = true;
+      avatar_path = "${config.home.homeDirectory}/.dotfiles/assets/avatar.jpg";
     };
   };
 }
