d9df35b2a2a9dc4a7d08af08f1f90cf72100abbc
giuxtaposition <yg97.cs@gmail.com>
2025-05-23 09:15:33 +0200

    feat(nvim): use underline in bufferline to better see current tab

--- .config/nvim/lua/plugins/ui/bufferline.lua
+++ .config/nvim/lua/plugins/ui/bufferline.lua
@@ -3,15 +3,6 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   event = "VeryLazy",
   version = "*",
-  keys = {
-    { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
-    { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
-    { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
-    { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
-    { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
-    { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-    { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
-  },
   opts = function()
     return {
       highlights = require("catppuccin.groups.integrations.bufferline").get(),
@@ -19,6 +10,7 @@ return {
         close_command = function(n)
           require("mini.bufremove").delete(n, false)
         end,
+        indicator = { style = "underline" },
         show_buffer_close_icons = false,
         show_close_icon = false,
         diagnostics = "nvim_lsp",
@@ -31,4 +23,13 @@ return {
       },
     }
   end,
+  keys = {
+    { "<leader>bp", "<cmd>BufferLinePick<cr>", desc = "Pick a buffer to open" },
+    { "<leader>bc", "<cmd>BufferLinePickClose<cr>", desc = "Select a buffer to close" },
+    { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
+    { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
+    { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
+    { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
+    { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
+  },
 }
