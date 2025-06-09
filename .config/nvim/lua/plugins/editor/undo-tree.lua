cd62ac528203b3bbbc72eed82ff6a96f0d3f6d22
giuxtaposition <yg97.cs@gmail.com>
2025-06-09 09:27:24 +0200

    feat(nvim): add undo-tree

---
+++ .config/nvim/lua/plugins/editor/undo-tree.lua
@@ -0,0 +1,11 @@
+return {
+  "mbbill/undotree",
+  lazy = false,
+  cmd = "UndotreeToggle",
+  init = function()
+    -- Settings
+  end,
+  keys = {
+    { "<leader>ut", "<cmd>UndotreeToggle<cr>", desc = "Toggle Undotree" },
+  },
+}
