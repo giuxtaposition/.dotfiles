f0a563764f71c0040eea7532d6686192ebc6486c
giuxtaposition <yg97.cs@gmail.com>
2025-07-18 14:38:06 +0200

    feat(wezterm): setup hyperlink_rules

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local ui = require("ui")
+local general = require("general")
 local keymaps = require("keymaps")
 
 local config = wezterm.config_builder()
@@ -7,6 +8,7 @@ local config = wezterm.config_builder()
 config.default_workspace = "home"
 
 ui.apply_to_config(config)
+general.apply_to_config(config)
 keymaps.apply_to_config(config)
 
 return config
