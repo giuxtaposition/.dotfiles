063870b81a1a4e646496f4afdda478291c0ce574
giuxtaposition <yg97.cs@gmail.com>
2026-01-29 18:01:02 +0100

    feat(neovim): use skim for fzf-lua

--- .config/nvim/lua/plugins/editor/fuzzy-finder.lua
+++ .config/nvim/lua/plugins/editor/fuzzy-finder.lua
@@ -1,140 +1,138 @@
-
 vim.pack.add({
   {
-    src = "https://github.com/nvim-tree/nvim-web-devicons"
+    src = "https://github.com/nvim-tree/nvim-web-devicons",
   },
   {
-    src = "https://github.com/ibhagwan/fzf-lua"
+    src = "https://github.com/ibhagwan/fzf-lua",
   },
 })
 
-
 local actions = require("fzf-lua.actions")
-    require("fzf-lua").setup{
-      defaults = {
-        prompt = " ",
-        header = false,
-        formatter = "path.filename_first",
-      },
-      winopts = {
-        height = 0.8,
-        width = 0.9,
-        border = "none",
-        preview = {
-          scrollbar = false,
-          horizontal = "right:50%",
-        },
-        on_create = function()
-          vim.keymap.set("t", "<C-j>", "<Down>", { silent = true, buffer = true })
-          vim.keymap.set("t", "<C-k>", "<Up>", { silent = true, buffer = true })
-        end,
-      },
-      fzf_opts = {
-        ["--no-info"] = "",
-        ["--info"] = "hidden",
-        ["--layout"] = "reverse-list",
-        ["--padding"] = "5%,5%,5%,5%",
-        ["--extended"] = "",
-      },
-      files = {
-        cwd_prompt = false,
-      },
-      grep = {
-        rg_opts = "--hidden --glob '!.git' --column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e",
-        rg_glob = true,
-        rg_glob_fn = function(query, opts)
-          local regex, flags = query:match("^(.-)%s%-%-(.*)$")
+require("fzf-lua").setup({
+  fzf_bin = "sk",
+  fzf_opts = {
+    ["--algo"] = "frizbee",
+    ["--no-info"] = "",
+    ["--info"] = "hidden",
+    ["--layout"] = "reverse-list",
+    ["--extended"] = "",
+  },
+  defaults = {
+    prompt = " ",
+    header = false,
+    formatter = "path.filename_first",
+  },
+  winopts = {
+    height = 0.8,
+    width = 0.9,
+    border = "none",
+    preview = {
+      scrollbar = false,
+      horizontal = "right:50%",
+    },
+    on_create = function()
+      vim.keymap.set("t", "<C-j>", "<Down>", { silent = true, buffer = true })
+      vim.keymap.set("t", "<C-k>", "<Up>", { silent = true, buffer = true })
+    end,
+  },
+  files = {
+    cwd_prompt = false,
+  },
+  grep = {
+    rg_opts = "--hidden --glob '!.git' --column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e",
+    rg_glob = true,
+    rg_glob_fn = function(query, opts)
+      local regex, flags = query:match("^(.-)%s%-%-(.*)$")
 
-          local function rebuild_regex(str)
-            local result = str:gsub("%s+", function(r)
-              if #r == 1 then
-                return ".*"
-              else
-                return r:match("%s(.*)")
-              end
-            end)
-            return result
+      local function rebuild_regex(str)
+        local result = str:gsub("%s+", function(r)
+          if #r == 1 then
+            return ".*"
+          else
+            return r:match("%s(.*)")
           end
-
-          if not regex then
-            return rebuild_regex(query), ""
-          end
-
-          return rebuild_regex(regex), flags
-        end,
-        glob_separator = "",
-      },
-      keymap = {
-        fzf = {
-          ["ctrl-q"] = "select-all+accept",
-        },
-      },
-      actions = {
-        files = {
-          ["enter"] = actions.file_edit_or_qf,
-          ["ctrl-x"] = actions.file_split,
-          ["ctrl-v"] = actions.file_vsplit,
-          ["ctrl-t"] = actions.file_tabedit,
-          ["alt-q"] = actions.file_sel_to_qf,
-        },
-      },
-      helptags = {
-        prompt = " ",
-        actions = {
-          -- Open help pages in a vertical split.
-          ["enter"] = actions.help_vert,
-        },
-      },
-      lsp = {
-        symbols = {
-          symbol_icons = require("config.ui.icons").kinds,
-        },
-      },
-      oldfiles = {
-        include_current_session = true,
-      },
-      buffers = {},
-      previewers = {
-        builtin = {
-          extensions = {
-            ["jpg"] = { "ueberzug" },
-            ["png"] = { "ueberzug" },
-            ["svg"] = { "ueberzug" },
-          },
-          -- if using `ueberzug` in the above extensions map
-          -- set the default image scaler, possible scalers:
-          --   false (none), "crop", "distort", "fit_contain",
-          --   "contain", "forced_cover", "cover"
-          -- https://github.com/seebye/ueberzug
-          ueberzug_scaler = "cover",
-          syntax_limit_b = 1024 * 100, -- 100KB
-        },
-      },
-    }
-
-
-    vim.ui.select = function(items, opts, on_choice)
-      local ui_select = require("fzf-lua.providers.ui_select")
-
-      -- Register the fzf-lua picker the first time we call select.
-      if not ui_select.is_registered() then
-        ui_select.register(function(ui_opts)
-          ui_opts.winopts = { height = 0.5, width = 0.4 }
-
-          -- Use the kind (if available) to set the previewer's title.
-          if ui_opts.kind then
-            ui_opts.winopts.title = string.format(" %s ", ui_opts.kind)
-          end
-
-          return ui_opts
         end)
+        return result
       end
 
-      -- Don't show the picker if there's nothing to pick.
-      if #items > 0 then
-        return vim.ui.select(items, opts, on_choice)
+      if not regex then
+        return rebuild_regex(query), ""
       end
-    end
+
+      return rebuild_regex(regex), flags
+    end,
+    glob_separator = "",
+  },
+  keymap = {
+    fzf = {
+      ["ctrl-q"] = "select-all+accept",
+    },
+  },
+  actions = {
+    files = {
+      ["enter"] = actions.file_edit_or_qf,
+      ["ctrl-x"] = actions.file_split,
+      ["ctrl-v"] = actions.file_vsplit,
+      ["ctrl-t"] = actions.file_tabedit,
+      ["alt-q"] = actions.file_sel_to_qf,
+    },
+  },
+  helptags = {
+    prompt = " ",
+    actions = {
+      -- Open help pages in a vertical split.
+      ["enter"] = actions.help_vert,
+    },
+  },
+  lsp = {
+    symbols = {
+      symbol_icons = require("config.ui.icons").kinds,
+    },
+  },
+  oldfiles = {
+    include_current_session = true,
+  },
+  buffers = {},
+  previewers = {
+    builtin = {
+      extensions = {
+        ["jpg"] = { "ueberzug" },
+        ["png"] = { "ueberzug" },
+        ["svg"] = { "ueberzug" },
+      },
+      -- if using `ueberzug` in the above extensions map
+      -- set the default image scaler, possible scalers:
+      --   false (none), "crop", "distort", "fit_contain",
+      --   "contain", "forced_cover", "cover"
+      -- https://github.com/seebye/ueberzug
+      ueberzug_scaler = "cover",
+      syntax_limit_b = 1024 * 100, -- 100KB
+    },
+  },
+})
+
+vim.ui.select = function(items, opts, on_choice)
+  local ui_select = require("fzf-lua.providers.ui_select")
+
+  -- Register the fzf-lua picker the first time we call select.
+  if not ui_select.is_registered() then
+    ui_select.register(function(ui_opts)
+      ui_opts.winopts = { height = 0.5, width = 0.4 }
+
+      -- Use the kind (if available) to set the previewer's title.
+      if ui_opts.kind then
+        ui_opts.winopts.title = string.format(" %s ", ui_opts.kind)
+      end
+
+      return ui_opts
+    end)
+  end
+
+  -- Don't show the picker if there's nothing to pick.
+  if #items > 0 then
+    return vim.ui.select(items, opts, on_choice)
+  end
+end
 
 local set_keymap = require("config.util.keys").set
 
@@ -193,4 +191,3 @@ set_keymap("n", "<leader>gS", function()
 end, "Find string in git status files")
 set_keymap("n", "z=", "<cmd>FzfLua spell_suggest<cr>", "Find spell word suggestion")
 set_keymap("n", "<leader>ut", "<cmd>FzfLua undotree<cr>", "Undotree")
-
--- modules/home-manager/terminal/fish.nix
+++ modules/home-manager/terminal/fish.nix
@@ -98,6 +98,14 @@
       enableFishIntegration = true;
     };
 
+    programs.skim = {
+      enable = true;
+    };
+
+    home.sessionVariables = {
+      SKIM_DEFAULT_OPTIONS = "--color=fg:#cdd6f4,bg:#1e1e2e,matched:#313244,matched_bg:#b4befe,current:#cdd6f4,current_bg:#45475a,current_match:#1e1e2e,current_match_bg:#89b4fa,spinner:#a6e3a1,info:#cba6f7,prompt:#89b4fa,cursor:#f38ba8,selected:#eba0ac,header:#94e2d5,border:#6c7086";
+    };
+
     programs.fzf = {
       enable = true;
       enableFishIntegration = true;
