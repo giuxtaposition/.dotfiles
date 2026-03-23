7b4e9d7dbee06b33cc10ac45f6aa9332dedc1bbc
giuxtaposition <yg97.cs@gmail.com>
2026-03-23 19:15:16 +0100

    feat(nixos): restore note sync

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -78,6 +78,7 @@
     obs-studio
     rclone
     trilium-desktop
+    obsidian
   ];
 
   # GTK CONFIG
@@ -104,6 +105,45 @@
   systemd.user = {
     # Nicely reload system units when changing configs
     startServices = "sd-switch";
+
+    services = {
+      sync-notes = {
+        Unit = {Description = "Sync notes with github repo";};
+        Service = {
+          Type = "forking";
+          Environment = "PATH=${
+            lib.makeBinPath [pkgs.openssh pkgs.gawk pkgs.git pkgs.libnotify]
+          }";
+          ExecStart = let
+            script = pkgs.writeShellScript "sync-notes" ''
+              echo "Syncing notes"
+              git pull --rebase --autostash
+              filesChanged=$(git status --porcelain | awk '{print $2}')
+              if [ -n "$filesChanged" ]; then
+              git add .
+              git commit -m "updating notes 📘"
+              git push
+              notify-send "Synced notes"
+              fi
+            '';
+          in "${pkgs.bash}/bin/bash ${script}";
+          WorkingDirectory = "${config.home.homeDirectory}/notes";
+        };
+        Install.WantedBy = ["default.target"];
+      };
+    };
+
+    timers = {
+      sync-notes = {
+        Unit.Description = "Timer for sync-notes service";
+        Timer = {
+          Unit = "sync-notes";
+          OnBootSec = "1m";
+          OnUnitActiveSec = "1h";
+        };
+        Install.WantedBy = ["timers.target"];
+      };
+    };
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
