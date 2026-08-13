ac8ef0713fe6ca837d40f29fcd2239ec198aa378
giuxtaposition <yg97.cs@gmail.com>
2026-08-13 10:09:54 +0200

    🐛 slack: sometimes crashing when screen sharing

--- overlays/default.nix
+++ overlays/default.nix
@@ -12,13 +12,11 @@
 
     slack = prev.slack.overrideAttrs (_oldAttrs: {
       fixupPhase = ''
-        sed -i -e 's/,"WebRTCPipeWireCapturer"/,"LebRTCPipeWireCapturer"/' $out/lib/slack/resources/app.asar
-
         rm $out/bin/slack
         makeWrapper $out/lib/slack/slack $out/bin/slack \
           --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
           --suffix PATH : ${prev.lib.makeBinPath [prev.pkgs.xdg-utils]} \
-          --add-flags "--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations,WebRTCPipeWireCapturer"
+          --add-flags "--ozone-platform=x11"
       '';
     });
   };
